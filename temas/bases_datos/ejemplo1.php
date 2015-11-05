250e007dcf79935b1b221296f644100a9228b5d2
José Manuel Gallegos Muro <jgallegos.innox@gmail.com>
2015-11-05 16:42:54 -0600

    Se corrigen datos de acceso a bd

--- temas/bases_datos/ejemplo1.php
+++ temas/bases_datos/ejemplo1.php
@@ -2,7 +2,7 @@
 
 //Conexiones con la base de datos
 
-$mysqli = new mysqli('127.0.0.1', 'root', '', "curso_php");
+$mysqli = new mysqli('127.0.0.1', 'curso_php', 'curso_phpii', "curso_php");
 
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
